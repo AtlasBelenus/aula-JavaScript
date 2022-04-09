479ea0841c2be3a043032fb5a54a569812e0cfe2
jorgeluizsffilho <jorgeluiz.sffilho@gmail.com>
2022-04-09 06:38:43 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
-var lista = ["Uva", "Jaca", "Amora"];
-//lista.push("Manga");
-//lista.pop();
+var date = Date();
+alert(date.getMinutes());
+
+/*
+var count;
+for(count = 1; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while(count < 5){
+    alert(count + 1);
+    count = count + 1;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?: ");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+
+
+/*
+var frutas = [{nome:"uva", cor:"roxa"}, {nome:"jaca", cor:"verde"}, {nome:"kiwi", cor:"marrom"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome:"uva", cor:"roxa"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["Uva", "Jaca", "Kiwi"];
+//lista.push("Manga"); // Coloca um item a mais na lista
+//lista.pop(); // Tira o ultimo elemento da lista
 //console.log(lista.reverse()); // Inverte a ordem dos objetos do Ultimo ao Primeiro
-//console.log(lista.length); // mostra o numero de objetos na lista
-console.log(lista.toString());
-alert(lista)
+//console.log(lista.length); // mostra o numero de elementos na lista
+//console.log(lista);  // exibe os elementos entre ()
+//console.log(lista.toString()); // Transforma a lista em string
+//console.log(lista.join(" 0 ")); // exibe a lista inserindo entre os elementos o que estiver em ()
+//alert(lista) // Exibe os elementos entre () na tela
